91599e6b3595612b1203a9373549dccb3f5a8fd2
Dan Cervelli <dan@closedcontour.com>
2018-04-16 20:08:12 -0700

    Prettier.

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,119 @@
-import { window, ExtensionContext, commands, Selection, Disposable }  from 'vscode';
-import { SelectionStrategy } from './api';
-import { TypescriptStrategy } from './languages/typescript';
+import {
+  window,
+  ExtensionContext,
+  commands,
+  Selection,
+  Disposable
+} from "vscode";
+import { SelectionStrategy } from "./api";
+import { TypescriptStrategy } from "./languages/typescript";
 
 export function activate(context: ExtensionContext) {
-    const verySmartSelect = new VerySmartSelect();
-    const growCommand = commands.registerCommand('very-smart-select.grow', () => {
-        verySmartSelect.grow();
-    });
-    const shrinkCommand = commands.registerCommand('very-smart-select.shrink', () => {
-        verySmartSelect.shrink();
-    });
+  const verySmartSelect = new VerySmartSelect();
+  const growCommand = commands.registerCommand("very-smart-select.grow", () => {
+    verySmartSelect.grow();
+  });
+  const shrinkCommand = commands.registerCommand(
+    "very-smart-select.shrink",
+    () => {
+      verySmartSelect.shrink();
+    }
+  );
 
-    context.subscriptions.push(growCommand);
-    context.subscriptions.push(shrinkCommand);
-    context.subscriptions.push(verySmartSelect);
+  context.subscriptions.push(growCommand);
+  context.subscriptions.push(shrinkCommand);
+  context.subscriptions.push(verySmartSelect);
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
 
-function areSelectionsEqual(selections: Selection[], otherSelections: Selection[]): boolean {
-    return selections.length === otherSelections.length
-        && selections.every((selection, index) => selection.isEqual(otherSelections[index]));
+function areSelectionsEqual(
+  selections: Selection[],
+  otherSelections: Selection[]
+): boolean {
+  return (
+    selections.length === otherSelections.length &&
+    selections.every((selection, index) =>
+      selection.isEqual(otherSelections[index])
+    )
+  );
 }
 
 class VerySmartSelect {
-    private strategies: { [key:string]: SelectionStrategy | undefined } = {};
+  private strategies: { [key: string]: SelectionStrategy | undefined } = {};
 
-    private selectionsHistory: Selection[][] = [];
-    private windowSelectionListener: Disposable;
-    private didUpdateSelections: boolean = false;
+  private selectionsHistory: Selection[][] = [];
+  private windowSelectionListener: Disposable;
+  private didUpdateSelections: boolean = false;
 
-    constructor() {
-        this.strategies["typescript"] = new TypescriptStrategy();
-        this.strategies["typescriptreact"] = new TypescriptStrategy();
-        this.strategies["javascript"] = new TypescriptStrategy();
-        this.strategies["javascriptreact"] = new TypescriptStrategy();
-        this.strategies["json"] = new TypescriptStrategy();
-        this.strategies["jsonc"] = new TypescriptStrategy();
+  constructor() {
+    this.strategies["typescript"] = new TypescriptStrategy();
+    this.strategies["typescriptreact"] = new TypescriptStrategy();
+    this.strategies["javascript"] = new TypescriptStrategy();
+    this.strategies["javascriptreact"] = new TypescriptStrategy();
+    this.strategies["json"] = new TypescriptStrategy();
+    this.strategies["jsonc"] = new TypescriptStrategy();
 
-        this.windowSelectionListener = window.onDidChangeTextEditorSelection(e => {
-            if (this.didUpdateSelections) {
-                this.didUpdateSelections = false;
-            } else {
-                this.selectionsHistory = [];
-            }
-        });
+    this.windowSelectionListener = window.onDidChangeTextEditorSelection(e => {
+      if (this.didUpdateSelections) {
+        this.didUpdateSelections = false;
+      } else {
+        this.selectionsHistory = [];
+      }
+    });
+  }
+
+  public grow() {
+    const editor = window.activeTextEditor;
+    if (!editor) {
+      return;
     }
-
-    public grow() {
-        const editor = window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-        const doc = editor.document;
-        const strategy = this.strategies[doc.languageId];
-        if (strategy === undefined) {
-            commands.executeCommand("editor.action.smartSelect.grow");
-            return;
-        }
-        const ranges = strategy.grow(editor);
-        const selections = ranges.map(range =>
-            new Selection(doc.positionAt(range.start), doc.positionAt(range.end))
-        );
-        this.updateSelectionsHistory(editor.selections);
-        this.updateSelections(selections);
+    const doc = editor.document;
+    const strategy = this.strategies[doc.languageId];
+    if (strategy === undefined) {
+      commands.executeCommand("editor.action.smartSelect.grow");
+      return;
     }
+    const ranges = strategy.grow(editor);
+    const selections = ranges.map(
+      range =>
+        new Selection(doc.positionAt(range.start), doc.positionAt(range.end))
+    );
+    this.updateSelectionsHistory(editor.selections);
+    this.updateSelections(selections);
+  }
 
-    public shrink() {
-        const selections = this.selectionsHistory.pop();
-        if (selections) {
-            this.updateSelections(selections);
-        } else {
-            commands.executeCommand("editor.action.smartSelect.shrink");
-        }
+  public shrink() {
+    const selections = this.selectionsHistory.pop();
+    if (selections) {
+      this.updateSelections(selections);
+    } else {
+      commands.executeCommand("editor.action.smartSelect.shrink");
     }
+  }
 
-    public dispose() {
-        this.windowSelectionListener.dispose();
-    }
+  public dispose() {
+    this.windowSelectionListener.dispose();
+  }
 
-    private updateSelections(selections: Selection[]) {
-        const editor = window.activeTextEditor;
-        if (editor && selections.length > 0) {
-            this.didUpdateSelections = true;
-            editor.selections = selections;
-        }
+  private updateSelections(selections: Selection[]) {
+    const editor = window.activeTextEditor;
+    if (editor && selections.length > 0) {
+      this.didUpdateSelections = true;
+      editor.selections = selections;
     }
+  }
 
-    private updateSelectionsHistory(selections: Selection[]) {
-        const lastSelections = this.selectionsHistory.length > 0
-            ? this.selectionsHistory[this.selectionsHistory.length - 1]
-            : undefined;
-        if (lastSelections === undefined || !areSelectionsEqual(lastSelections, selections)) {
-            this.selectionsHistory.push([...selections]);
-        }
+  private updateSelectionsHistory(selections: Selection[]) {
+    const lastSelections =
+      this.selectionsHistory.length > 0
+        ? this.selectionsHistory[this.selectionsHistory.length - 1]
+        : undefined;
+    if (
+      lastSelections === undefined ||
+      !areSelectionsEqual(lastSelections, selections)
+    ) {
+      this.selectionsHistory.push([...selections]);
     }
+  }
 }
--- src/languages/typescript.ts
+++ src/languages/typescript.ts
@@ -2,106 +2,111 @@ import { SelectionStrategy, Range } from "../api";
 import { TextEditor } from "vscode";
 import { createSourceFile, Node, ScriptTarget } from "typescript";
 
-function pathToPositionInternal(node: Node, start: number, end: number, path: Node[]) {
-    const nodeStart = node.getFullStart();
-    const nodeEnd = node.getEnd();
-    if (start < nodeStart || end > nodeEnd) {
-        return;
-    }
-    path.push(node);
-    node.forEachChild(child => {
-        pathToPositionInternal(child, start, end, path);
-    });
+function pathToPositionInternal(
+  node: Node,
+  start: number,
+  end: number,
+  path: Node[]
+) {
+  const nodeStart = node.getFullStart();
+  const nodeEnd = node.getEnd();
+  if (start < nodeStart || end > nodeEnd) {
+    return;
+  }
+  path.push(node);
+  node.forEachChild(child => {
+    pathToPositionInternal(child, start, end, path);
+  });
 }
 
 function pathToPosition(node: Node, start: number, end: number): Node[] {
-    const path: Node[] = [];
-    pathToPositionInternal(node, start, end, path);
-    return path;
+  const path: Node[] = [];
+  pathToPositionInternal(node, start, end, path);
+  return path;
 }
 
 const WHITESPACE = /\s/;
 
 // TODO: if you start in whitespace on either side, expand until you include non-whitespace
 function expandWhitespace(source: string, range: Range): Range {
-    let i = range.start - 1;
-    let leftAdd = 0;
-    while (i >= 0 && WHITESPACE.test(source.charAt(i))) {
-        i--;
-        leftAdd++;
-    }
-    let j = range.end + 1;
-    let rightAdd = 0;
-    while (j < source.length && WHITESPACE.test(source.charAt(j))) {
-        j++;
-        rightAdd++;
-    }
-    return {
-        start: range.start - leftAdd,
-        end: range.end + rightAdd,
-    };
+  let i = range.start - 1;
+  let leftAdd = 0;
+  while (i >= 0 && WHITESPACE.test(source.charAt(i))) {
+    i--;
+    leftAdd++;
+  }
+  let j = range.end + 1;
+  let rightAdd = 0;
+  while (j < source.length && WHITESPACE.test(source.charAt(j))) {
+    j++;
+    rightAdd++;
+  }
+  return {
+    start: range.start - leftAdd,
+    end: range.end + rightAdd
+  };
 }
 
 function collapseWhitespace(source: string, range: Range): Range {
-    let i = range.start;
-    let leftRemove = 0;
-    while (i < source.length && WHITESPACE.test(source.charAt(i))) {
-        i++;
-        leftRemove++;
-    }
-    let j = range.end - 1;
-    let rightRemove = 0;
-    while (j >= 0 && WHITESPACE.test(source.charAt(j))) {
-        j--;
-        rightRemove++;
-    }
-    return {
-        start: range.start + leftRemove,
-        end: range.end - rightRemove,
-    };
+  let i = range.start;
+  let leftRemove = 0;
+  while (i < source.length && WHITESPACE.test(source.charAt(i))) {
+    i++;
+    leftRemove++;
+  }
+  let j = range.end - 1;
+  let rightRemove = 0;
+  while (j >= 0 && WHITESPACE.test(source.charAt(j))) {
+    j--;
+    rightRemove++;
+  }
+  return {
+    start: range.start + leftRemove,
+    end: range.end - rightRemove
+  };
 }
 
 export function nodeToRange(node: Node): Range {
-    return {
-        start: node.getFullStart(),
-        end: node.getEnd(),
-    };
+  return {
+    start: node.getFullStart(),
+    end: node.getEnd()
+  };
 }
 
 export class TypescriptStrategy implements SelectionStrategy {
-    private expandWhitespace = false;
+  private expandWhitespace = false;
 
-    grow(editor: TextEditor): Range[] {
-        const doc = editor.document;
-        const startRanges = editor.selections.map(selection => ({
-            start: doc.offsetAt(selection.start),
-            end: doc.offsetAt(selection.end),
-        }));
-        const text = doc.getText();
-        const ranges = this.expandWhitespace 
-            ? startRanges.map(range => expandWhitespace(text, range)) 
-            : startRanges;
-        const node = createSourceFile(doc.fileName, text, ScriptTarget.Latest);
-        const outRanges = ranges
-            .map(range => {
-                const path = pathToPosition(node, range.start, range.end);
-                let expansionNode: Node | undefined;
-                for (let i = path.length - 1; i >= 0; i--) {
-                    const candidate = path[i];
-                    const outRange = collapseWhitespace(text, nodeToRange(candidate));
-                    if (outRange.start < range.start || outRange.end > range.end) {
-                        expansionNode = candidate;
-                        break;
-                    }
-                }
-                if (expansionNode === undefined) {
-                    return undefined;
-                }
-                const outRange = collapseWhitespace(text, nodeToRange(expansionNode));
-                return outRange;
-            })
-            .filter(range => range !== undefined)
-            .map(range => range!);
-        return outRanges;
-    }
+  grow(editor: TextEditor): Range[] {
+    const doc = editor.document;
+    const startRanges = editor.selections.map(selection => ({
+      start: doc.offsetAt(selection.start),
+      end: doc.offsetAt(selection.end)
+    }));
+    const text = doc.getText();
+    const ranges = this.expandWhitespace
+      ? startRanges.map(range => expandWhitespace(text, range))
+      : startRanges;
+    const node = createSourceFile(doc.fileName, text, ScriptTarget.Latest);
+    const outRanges = ranges
+      .map(range => {
+        const path = pathToPosition(node, range.start, range.end);
+        let expansionNode: Node | undefined;
+        for (let i = path.length - 1; i >= 0; i--) {
+          const candidate = path[i];
+          const outRange = collapseWhitespace(text, nodeToRange(candidate));
+          if (outRange.start < range.start || outRange.end > range.end) {
+            expansionNode = candidate;
+            break;
+          }
+        }
+        if (expansionNode === undefined) {
+          return undefined;
+        }
+        const outRange = collapseWhitespace(text, nodeToRange(expansionNode));
+        return outRange;
+      })
+      .filter(range => range !== undefined)
+      .map(range => range!);
+    return outRanges;
+  }
 }
